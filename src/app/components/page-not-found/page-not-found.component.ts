b7c3fdc4eaab8bc7062112a1e70b5a97d676bb74
TshepoMasuku <tshepomasuku16@gmail.com>
2024-09-10 15:47:53 +0200

    component html+css done

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
