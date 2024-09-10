cbfdf58be3eb4dab1a18e43f8e24fe030dbbfab1
TshepoMasuku <tshepomasuku16@gmail.com>
2024-09-10 14:02:13 +0200

    created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about',  component: AboutComponent},
+    {path: 'projects',  component: ProjectsComponent},
+    {path: 'contact',  component: ContactComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
