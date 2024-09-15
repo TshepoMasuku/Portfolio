fb190c1610953670545b9420578889cc94b832c0
TshepoMasuku <tshepomasuku16@gmail.com>
2024-09-15 04:18:47 +0200

    responsive improvements

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
